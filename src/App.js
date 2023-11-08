e941ebd97e8faa6efa1eb2ce700d893b2363f3cb
Achraf Mansari <contact@achrafmansari.com>
2023-11-08 22:53:10 +0100

    Updated Google Analytics implementation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
+import ReactGA from "react-ga";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import MajorPage from "./pages/MajorPage";
 import SemesterPage from "./pages/SemesterPage";
-import PdfViewPage from "./pages/PdfViewPage";
 import Error404Page from "./pages/Error404Page";
 
+ReactGA.initialize(process.env.GA_TRACKING_ID);
+
 function App() {
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <div className="App min-h-screen flex flex-col">
 
